bf2883fde7b27ddf8c32cb3f947ea65c5740654e
portothree <portothree@gmail.com>
2019-02-19 15:59:35 -0300

    loaded player image to the surface and centered it on the screen

--- rpg_game.py
+++ rpg_game.py
@@ -16,6 +16,10 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
 pygame.display.set_caption(SCREEN_TITLE)
 
+player_image = pygame.image.load('player.png')
+player_image = pygame.transform.scale(player_image, (50, 50))
+
+
 while not is_game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,7 +27,12 @@ while not is_game_over:
 
     print(event)
 
-    pygame.draw.rect(game_screen, BLACK_COLOR, [400, 400, 100, 100])
+    # Rectangle (x, y, width, height)
+    # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
+    # Circle (x, y, radius)
+    # pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+
+    game_screen.blit(player_image, (375, 375))
 
     pygame.display.update()
     clock.tick(TICK_RATE)
